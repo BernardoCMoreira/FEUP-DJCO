bcfad8e23c19b8ea746515eef0e8d7e5bc8717ba
BernardoCMoreira <up201604014@fe.up.pt>
2021-03-13 18:04:30 +0000

    Added new level, with a scroll. Need to change player position when door opens

---
+++ Feup-Ops/Assets/Scripts/Door.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Door : MonoBehaviour
+{
+    [SerializeField] Text Message;
+    private bool secretLevel;
+
+    void Start(){
+        secretLevel = false;
+    }
+    void OnTriggerEnter2D(Collider2D col){
+        
+        if(col.CompareTag("Player")){
+            Message.text = ("[O] to Open the door! ");
+              if(Input.GetKeyDown("o")){
+                Debug.Log("Door opened");
+            }
+        }
+    }
+
+    void OnTriggerStay2D(Collider2D col){
+        if (col.CompareTag("Player")){
+            if(Input.GetKeyDown("o")){
+                Debug.Log("Door opened");
+            }
+        }
+    }
+
+    void OnTriggerExit2D(Collider2D col){
+        if (col.CompareTag("Player")){
+            Message.text = ("");
+        }
+    }
+}
