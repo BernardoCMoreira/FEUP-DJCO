40b8973c11407da3c1099a4ecb86a831f7b54e2c
BernardoCMoreira <up201604014@fe.up.pt>
2021-03-21 15:43:40 +0000

    added on trigger enter

--- Feup-Ops/Assets/Scripts/Door.cs
+++ Feup-Ops/Assets/Scripts/Door.cs
@@ -8,9 +8,13 @@ public class Door : MonoBehaviour
     public Text Message;
     public Player player;
 
+    void OnTriggerEnter2D(Collider2D col){
+        if(!col.CompareTag("Player")) return;
+        action();
+    }
+
     void OnTriggerStay2D(Collider2D col){
         if(!col.CompareTag("Player")) return;
-
         action();
     }
     
